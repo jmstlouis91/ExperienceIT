5c403c5e5461e5d55402693d756933128eb59d9b
jmstlouis91 <jmstlouis91@gmail.com>
2019-10-17 17:16:15 -0400

    sample

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int myNumber = 1; myNumber <=100; myNumber++) {
+            if ((myNumber % 3 == 0) && (myNumber % 5 == 0)) {
+                System.out.println("fizzbuzz");
+            } else if (myNumber % 5 == 0) {
+                System.out.println("buzz");
+            } else if (myNumber % 3 == 0) {
+                System.out.println("fizz");
+            } else {
+                System.out.println(myNumber);
+            }
+
+        }
+
+    }
+
+}
+
+
